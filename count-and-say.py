c65d72f31cb45a610da02919401587ea02987bc4
Aneka-zera <147018768+Aneka-zera@users.noreply.github.com>
2025-07-27 09:49:21 +0530

    Create count-and-say.py

---
+++ count-and-say.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        
+        if n==1:
+            return "1"
+        prev=self.countAndSay(n-1)
+        res=[]
+        count=1
+        for i in range(1,len(prev)):
+            if(prev[i]==prev[i-1]):
+                count+=1
+            else:
+                res.append(str(count)+prev[i-1])
+                count=1
+        res.append(str(count)+prev[-1])
+        return ''.join(res)     
+                
+
+        
