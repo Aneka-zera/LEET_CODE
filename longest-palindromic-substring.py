b6456c9e347b04dc276a7d3d7d80c467746e326c
Aneka-zera <147018768+Aneka-zera@users.noreply.github.com>
2025-11-26 09:44:41 +0530

    Implement longest palindromic substring solution

---
+++ longest-palindromic-substring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s or len(s) < 1:
+            return ""
+
+        start, end = 0, 0
+
+        for i in range(len(s)):
+            len1 = self.expand(s, i, i)       # odd length center
+            len2 = self.expand(s, i, i + 1)   # even length center
+            length = max(len1, len2)
+
+            if length > (end - start):
+                start = i - (length - 1) // 2
+                end = i + length // 2
+
+        return s[start:end+1]
+
+    def expand(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
